6e2917252a6d89be37af1ce21b9254b497fc60b6
Dan Streeter <dan@danstreeter.co.uk>
2019-05-04 21:27:02 +0100

    Added second route parameter for hours

--- index.py
+++ index.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Please give me some seconds!"
+    return "Please give me some seconds, and/or the hours per day your work day is, in /seconds/hours_per_day format"
 
 @app.route('/<int:seconds>')
-def seconds(seconds):
-    response = sec.ddhhmmss(seconds)
+@app.route('/<int:seconds>/<hours>')
+def seconds(seconds, hours=7.5):
+    response = sec.ddhhmmss(seconds, hours)
     return response
 
 @app.route('/version')
 def version():
-    return "0.0.1"
+    return "0.0.2"
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
